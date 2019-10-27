d9df9ed646acbe01e5f4244c21a74ea53c731cdc
kensuzuki <kenichi.suzuki.c@jp.ricoh.com>
2019-10-27 13:46:07 +0900

    refetchQuery

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ADD_STAR, REMOVE_STAR, SEARCH_REPOSITORIES } from './graphql'
 import { form } from 'zen-observable';
 
 const StarButton = props => {
-  const node = props.node
+  const { node, query, first, last, before, after } = props
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starCount = totalCount === 1 ? `1 Star` : `${totalCount} Stars`
@@ -26,7 +26,17 @@ const StarButton = props => {
     )
   }
   return (
-    <Mutation mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}>
+    <Mutation
+      mutation={viewerHasStarred ? REMOVE_STAR : ADD_STAR}
+      refetchQueries= {
+        [
+          {
+            query: SEARCH_REPOSITORIES,
+            variables: { query, first, last, before, after }
+          }
+        ]
+      }
+    >
       {
         arStar => <StarStatus arStar={arStar} />
       }
@@ -108,7 +118,7 @@ class App extends Component {
                         <li key={node.id}>
                           <a href={node.url} rel="noopener noreferrer" target="_blank">{node.name}</a>
                           &nbsp;
-                          <StarButton node={node}/>
+                          <StarButton node={node} {...{ query, first, last, before, after }}/>
                         </li>
                         )
                       })
